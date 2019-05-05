56919eabaef4692d17490a9d4613e48ee705bcc3
rjbowen <rjbowen97@gmail.com>
2019-05-05 18:13:16 -0400

    Clean slate to try to wire up the whole api

--- extractionTest.js
+++ extractionTest.js
@@ -1,16 +1,16 @@
 let xml2js = require('xml2js');
 let fs = require('fs');
 
-fs.readFile('thing1.json', 'utf8', (err, thing1AsString) => {
+fs.readFile('vpl1128.json', 'utf8', (err, thing1AsString) => {
     console.log(thing1AsString);
 
     let thing1AsObject = JSON.parse(thing1AsString);
 
-    let builder = new xml2js.Builder();
-    let thing1AsXMLString = builder.buildObject(thing1AsObject);
+    let xml2jsBuilder = new xml2js.Builder();
+    let vpl1128String = xml2jsBuilder.buildObject(thing1AsObject);
 
 
-    fs.writeFile('thing1AsXML.xml', thing1AsXMLString, err => {
+    fs.writeFile('vpl1128.xml', vpl1128String, err => {
         console.log(err);
     });
 });
