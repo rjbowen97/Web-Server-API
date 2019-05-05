9134e7bf59ddc5111e850cdb14632b92301eac72
rjbowen <rjbowen97@gmail.com>
2019-05-05 18:39:17 -0400

    Querying and sanitization appear to be working

--- app.js
+++ app.js
@@ -3,50 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
 var questions = require('./routes/questions');
-var tunnel = require('tunnel-ssh');
-var prompt = require('prompt');
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 var cors = require('cors');
-
-var sshCredentialPromptConfiguration = {
-  properties: {
-    sshUsername: {
-      description: 'Enter ssh username (EECIS username)',
-      type: 'string',
-      required: true,
-    },
-    sshPassword: {
-      description: 'Enter ssh password (EECIS password)',
-      type: 'string',
-      required: true,
-      hidden: true
-    }
-  }
-};
-
-prompt.get(sshCredentialPromptConfiguration, function (err, result) {
-  let sshTunnelConfig = {
-    username: result.sshUsername,
-    host: 'cisc475-7.cis.udel.edu',
-    agent: process.env.SSH_AUTH_SOCK,
-    port: 22,
-    dstPort: 27017,
-    password: result.sshPassword
-  };
-
-  let server = tunnel(sshTunnelConfig, function (error, server) {
-    if (error) {
-      console.log("SSH connection error: " + error);
-    }
-  });
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- extractionTest.js
+++ extractionTest.js
@@ -1,5 +1,8 @@
 let xml2js = require('xml2js');
 let fs = require('fs');
+var mongodb = require('mongodb');
+var tunnel = require('tunnel-ssh');
+var prompt = require('prompt');
 
 function desanitizeKey(key) {
     return key.replace(/\\D/g, '$').replace(/\\d/g, '.').replace(/\\b/g, '\\');
@@ -16,16 +19,52 @@ function desanitizeObject(object) {
     return ret;
 }
 
-fs.readFile('vpl1128.json', 'utf8', (err, thing1AsString) => {
-    console.log(thing1AsString);
 
-    let thing1AsObject = JSON.parse(thing1AsString);
+var sshCredentialPromptConfiguration = {
+    properties: {
+        sshUsername: {
+            description: 'Enter ssh username (EECIS username)',
+            type: 'string',
+            required: true,
+        },
+        sshPassword: {
+            description: 'Enter ssh password (EECIS password)',
+            type: 'string',
+            required: true,
+            hidden: true
+        }
+    }
+};
 
-    let xml2jsBuilder = new xml2js.Builder();
-    let vpl1128String = xml2jsBuilder.buildObject(thing1AsObject);
+prompt.get(sshCredentialPromptConfiguration, function (err, result) {
+    let sshTunnelConfig = {
+        username: result.sshUsername,
+        host: 'cisc475-7.cis.udel.edu',
+        agent: process.env.SSH_AUTH_SOCK,
+        port: 22,
+        dstPort: 27017,
+        password: result.sshPassword
+    };
 
+    let server = tunnel(sshTunnelConfig, function (error, server) {
+        if (error) {
+            console.log("SSH connection error: " + error);
+        }
 
-    fs.writeFile('vpl1128.xml', vpl1128String, err => {
-        console.log(err);
+        let MongoClient = require('mongodb').MongoClient;
+
+        // Initialize connection once
+        MongoClient.connect("mongodb://localhost:27017", { "useNewUrlParser": true }, function (error, client) {
+            if (error) {
+                return console.error(error);
+            }
+
+            let questionsUsingXml2jsCollection = client.db('autoexam').collection('questionsUsingXml2js');
+
+            questionsUsingXml2jsCollection.find({}).toArray(function (err, items) {
+                let desanitizedObject = desanitizeObject(items[0]);                
+                console.log(desanitizedObject);
+            });
+        });
     });
 });
