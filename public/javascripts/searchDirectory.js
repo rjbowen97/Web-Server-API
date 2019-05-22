2d8b063a29cb006f7b319d3fc17509971f011a88
rjbowen <rjbowen97@gmail.com>
2019-05-22 19:31:04 -0400

    Got rid of extraneous files

--- public/javascripts/searchDirectory.js
+++
@@ -1,8 +0,0 @@
-function searchDirectory(){
-    var fs = require('fs');
-    //TODO: Find the file without hard-coding it. Then call this function in xmltojson.
-        //Currently not in use.
-    return 'moodle.mbz.uncompressed/activities/vpl_49/module.xml';
-}
-
-module.exports.searchDirectory = searchDirectory; // export your functuion
